fc0bdfe5c3e57755e84dfab4bee916ada297d3c5
KseniyaMyagkova <kseniya-myagkova@yandex.ru>
2024-07-29 18:49:08 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,10 @@
+print ("1st program")
+print(9**0.5*5)
+print("2nd program")
+print (9.99>9.98 and 1000!=1000.1)
+print ("3rd program")
+print(1234//10%100+5678//10%100)
+print("4th program")
+print(int(13.42)==int(42.13%42*100) or int(42.13)==int(13.42%13*100))
+
+
