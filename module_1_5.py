468329654ef1c3f410f77c8e980ee8a91274f3b0
KseniyaMyagkova <kseniya-myagkova@yandex.ru>
2024-08-01 12:55:53 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,10 @@
+immutable_var_=1,2,3,'Ks'
+print(immutable_var_)
+immutable_var_[3]='Ar'
+print(immutable_var_)
+mutable_list=[4,5,6]
+print(mutable_list)
+mutable_list[0]=1
+print(mutable_list)
+
+
