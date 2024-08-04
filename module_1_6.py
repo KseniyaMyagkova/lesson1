5269e7e7e76574e7c298c696ac9f0d040ef863cd
KseniyaMyagkova <kseniya-myagkova@yandex.ru>
2024-08-04 08:16:50 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,13 @@
+my_dict={'Kseniya':1980,'Artem':1979,'Vanya':2019}
+print(my_dict)
+print(my_dict['Kseniya'])
+print(my_dict.get('Olga'))
+my_dict.update({'Max':2000,'Alex':2001})
+my_dict.pop('Kseniya')
+print(my_dict)
+my_set={1,2,3,4,1,2,3}
+print(my_set)
+print(my_set.add(5),my_set.add(7))
+print(my_set.discard(1))
+print(my_set)
+
