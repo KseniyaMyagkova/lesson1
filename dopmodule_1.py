7301eaf00019f80a61dffa2c89f705a3ea4ce644
KseniyaMyagkova <kseniya-myagkova@yandex.ru>
2024-08-06 11:39:16 +0300

    Update dopmodule_1.py

--- dopmodule_1.py
+++ dopmodule_1.py
@@ -2,15 +2,19 @@ grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 list_=(sorted(students))
 print(list_)
+def average(*args): return([sum(x) / len(x) for x in args])
+a = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+b=(average(*a))
+result = [int(item) for item in b]
+c=result
+print(c)
 print("Original key list is : " + str(list_))
-print("Original value list is : " + str(grades))
+print("Original value list is : " + str(c))
 res = {}
 for key in list_:
-    for value in grades:
+    for value in c:
         res[key] = value
-        grades.remove(value)
+        c.remove(value)
         break
-
-# Printing resultant dictionary
 print("Resultant dictionary is : " + str(res))
 
